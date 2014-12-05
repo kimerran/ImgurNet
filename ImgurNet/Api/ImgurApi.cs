421ae1a3d14340db5d9743a9b3291cd279c8ce25
markhughneri <mark@oxygenventures.com.au>
2014-12-05 16:22:53 +0800

    structure ImgurApi to several partial classes

--- ImgurNet/Api/ImgurApi.cs
+++ ImgurNet/Api/ImgurApi.cs
@@ -9,7 +9,7 @@ using Likja.ImgurNet.Models;
 
 namespace Likja.ImgurNet.Api
 {
-    public class ImgurApi
+    public partial class ImgurApi
     {
         private readonly string _clientId;
         private readonly string _clientSecret;
@@ -18,57 +18,6 @@ namespace Likja.ImgurNet.Api
         {
             _clientId = clientId;
             _clientSecret = clientSecret;
-        }
-
-        /// <summary>
-        /// Get information about an image.
-        /// </summary>
-        /// <remarks>See: https://api.imgur.com/endpoints/image#image</remarks>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public async Task<Image> ImageDetails(string id)
-        {
-            var url = ImgurUrls.ImageIdUrl(id);
-
-            try
-            {
-                var result = await url.WithImgurAuthHeader(_clientId)
-                    .GetJsonAsync();
-
-                var basic = Mapper.Map<Basic>(result);
-                return Mapper.Map<Image>(basic.Data);
-            }
-            catch (Exception ex)
-            {
-                throw new RequestFailedException(ex);
-            }
-
-        }
-
-        /// <summary>
-        /// Upload a new image.
-        /// </summary>
-        /// <remarks>See: https://api.imgur.com/endpoints/image#image-upload</remarks>
-        /// <param name="image"></param>
-        /// <returns></returns>
-        public async Task<Image> ImageUpload(ImageUpload image)
-        {
-            var url = ImgurUrls.ImageUrl();
-
-            try
-            {
-                var result = await url.WithImgurAuthHeader(_clientId)
-                    .PostJsonAsync(image)
-                    .ReceiveJson();
-
-                var basic = Mapper.Map<Basic>(result);
-                return Mapper.Map<Image>(basic.Data);
-            }
-            catch (Exception ex)
-            {
-                throw new RequestFailedException(ex);
-            }
-
         }
     }
 }
---
+++ ImgurNet/Api/ImgurApiImage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Likja.ImgurNet.Common;
+using Likja.ImgurNet.Exceptions;
+using Likja.ImgurNet.Extensions;
+using Likja.ImgurNet.Mapping;
+using Likja.ImgurNet.Models;
+
+namespace Likja.ImgurNet.Api
+{
+    public partial class ImgurApi
+    {
+        /// <summary>
+        /// Get information about an image.
+        /// </summary>
+        /// <remarks>See: https://api.imgur.com/endpoints/image#image</remarks>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Image> ImageDetails(string id)
+        {
+            var url = ImgurUrls.ImageIdUrl(id);
+
+            try
+            {
+                var result = await url.WithImgurAuthHeader(_clientId)
+                    .GetJsonAsync();
+
+                var basic = Mapper.Map<Basic>(result);
+                return Mapper.Map<Image>(basic.Data);
+            }
+            catch (Exception ex)
+            {
+                throw new RequestFailedException(ex);
+            }
+
+        }
+
+        /// <summary>
+        /// Upload a new image.
+        /// </summary>
+        /// <remarks>See: https://api.imgur.com/endpoints/image#image-upload</remarks>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public async Task<Image> ImageUpload(ImageUpload image)
+        {
+            var url = ImgurUrls.ImageUrl();
+
+            try
+            {
+                var result = await url.WithImgurAuthHeader(_clientId)
+                    .PostJsonAsync(image)
+                    .ReceiveJson();
+
+                var basic = Mapper.Map<Basic>(result);
+                return Mapper.Map<Image>(basic.Data);
+            }
+            catch (Exception ex)
+            {
+                throw new RequestFailedException(ex);
+            }
+
+        }
+    }
+}
